3f4b72ffea1fdd157a4073ca1fbaa182e0387368
Mehdi Mirza <memirzamo@gmail.com>
2013-04-29 17:16:19 -0400

    FIX: resize redundant bug

--- scripts/mirzamom/resize.py
+++ scripts/mirzamom/resize.py
@@ -1,12 +1,24 @@
 import os
 import glob
+import numpy
 from skimage import transform, io
+from gif import sort_nicely
+import ipdb
 
-def resize(input, output, size):
+def resize(input, size):
 
     img = io.imread(input)
     img = transform.resize(img, size)
-    io.imsave(output, img)
+    return img
+
+
+def make_face_tubes(input, output):
+
+    if format == 'jpg':
+        io.imsave(output, img)
+    else:
+        output = output.rstrip('jpg') + '.npy'
+        numpy.save(output, img)
 
 if __name__ == "__main__":
 
@@ -25,6 +37,20 @@ if __name__ == "__main__":
                 os.makedirs(cur_dest)
 
             files = glob.glob("{}{}/{}/*.jpg".format(src, set_n, emot))
-            for input in files:
-                output = "{}{}".format(cur_dest, input.split("/")[-1])
-                resize(input, output, size)
+            clips = [item.split("/")[-1].split("-")[0] for item in files]
+            clips = numpy.unique(clips)
+            for clip in clips:
+                print clip
+                tubes = glob.glob("{}{}/{}/{}*.jpg".format(src, set_n, emot, clip))
+                tubes = [item.split("/")[-1].split("-")[1] for item in tubes]
+                tubes = numpy.unique(tubes)
+                for tube in tubes:
+                    frames = glob.glob("{}{}/{}/{}-{}*.jpg".format(src, set_n, emot, clip, tube))
+                    sort_nicely(frames)
+                    rval = []
+                    for frame in frames:
+                        rval.append(resize(frame, size))
+
+                    rval = numpy.concatenate([item[numpy.newaxis, ...] for item in rval])
+                    output = "{}{}/{}/{}-{}.npy".format(dest, set_n, emot, clip, tube)
+                    numpy.save(output, rval)
