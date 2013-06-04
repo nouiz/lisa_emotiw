877564402f97541866c54ae595b0d43e0b04f0d6
Abhishek Aggarwal <abhiggarwal@gmail.com>
2013-06-04 13:15:26 -0400

    change

--- scripts/aggarwal/faceHist.py
+++ scripts/aggarwal/faceHist.py
@@ -3,8 +3,10 @@
 import os
 import csv
 import pylab as P
+import matplotlib.image as mpimg
 
 path = '/data/lisa/data/faces/EmotiW/picasa_boxes/Train'
+pathImages = '/data/lisa/data/faces/EmotiW/images/Train'
 bbox = []
 heights = []
 widths = []
@@ -15,6 +17,11 @@ for root, subdirs, files in os.walk(path):
     for file in files:
         if os.path.splitext(file)[1].lower() in ('.txt'):
             filePath = os.path.join(root, file)
+            folder =  filePath.split('/')[-2]
+            image = os.path.splitext(filePath.split('/')[-1])[0]+'.png'
+            fileImage = os.path.join(pathImages, folder, image)
+            #img=mpimg.imread(fileImage)
+            #print img.shape
             data = open (filePath,"r")
             c = csv.reader(data, delimiter=',', skipinitialspace=True)
             for line in c:
@@ -23,8 +30,8 @@ for root, subdirs, files in os.walk(path):
                 bb = []
                 for val in line:
                     bb.append(int(val))
-                heights.append(bb[3]-bb[1])
-                widths.append(bb[2]-bb[0])
+                heights.append((bb[3]-bb[1])/576.0)
+                widths.append((bb[2]-bb[0])/1024.0)
                 bbox.append(bb)
                 
 
@@ -32,7 +39,7 @@ for root, subdirs, files in os.walk(path):
 print len(heights)
 
 # the histogram of the data with histtype='step'
-n, bins, patches = P.hist(heights, 100, normed=0, histtype='bar')
+n, bins, patches = P.hist(widths, 100, normed=0, histtype='bar')
 P.setp(patches, 'facecolor', 'r', 'alpha', 0.75)
 P.show()
 
