327fb0ea30a77e0e19928106005672adc0ba470d
Caglar <ca9lar@gmail.com>
2013-06-05 12:45:30 -0400

    Multiscale cascade ensembles.

--- emotiw/caglar/multiscale_cascade_ensembles.py
+++ emotiw/caglar/multiscale_cascade_ensembles.py
@@ -220,27 +220,26 @@ class MultiScaleCascadedDetectorEnsembles(Ensemble, Detector):
                 else:
                     #Remove the spatially close entries, hence check the, check the neighbours
                     #within radius neighbourhood.
+
                     #The problem is that the spatially close outputs might be corresponding to the same face.
                     #Rows:
                     r = j % self.output_map_shp[1]
+
                     #Columns:
                     c = j - r * self.output_map_shp[1]
                     iterator = circle_around(r, c)
 
                     #Move on the grid in a circular fashion
                     for loc in iterator:
-
                         if loc[0] == radius:
                             break
-
                         y, x = loc[1]
                         n = y * self.output_map_shp[0] + self.output_map_shp[1]
-
                         if (loc[1][0] >= 0 and loc[1][0] <= self.img_shape[0] and loc[1][1] <= self.img_shape[1] and loc[1][1] >= 0):
                             if (new_preds[pred_pos[n, 0]] < new_preds[pred_pos[j, 0]]):
                                 new_preds[pred_pos[n, 0]] = 0
                 processed_probs.append(new_preds)
-            processed_probs = numpy.asarray(processed_probs)
+            processed_probs = np.asarray(processed_probs)
         return processed_probs
 
     def _check_non_max_suppression(self, probs):
