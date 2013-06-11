9116bcf07e339db666ee1699845b910fc8fbe198
Abhishek Aggarwal <abhiggarwal@gmail.com>
2013-06-11 12:40:25 -0400

    Nicks keypoint detector

---
+++ emotiw/abhi/kpd/KeypointSGD.py
@@ -0,0 +1,183 @@
+from sgd import SGD
+import numpy as np
+from pylearn2.monitor import Monitor
+from theano import config
+import theano.tensor as T
+from theano import function
+from theano.gof.op import get_debug_values
+from theano.printing import Print
+import logging
+log = logging.getLogger(__name__)
+from pylearn2.utils import sharedX
+from pylearn2.utils.timing import log_timing
+
+
+class KeypointSGD(SGD):
+    """
+    Stochastic Gradient Descent
+    WRITEME: what is a good reference to read about this algorithm?
+
+    A TrainingAlgorithm that does gradient descent on minibatches.
+
+    """
+    def setup(self, model, dataset):
+
+        if self.cost is None:
+            self.cost = model.get_default_cost()
+
+        inf_params = [ param for param in model.get_params() if np.any(np.isinf(param.get_value())) ]
+        if len(inf_params) > 0:
+            raise ValueError("These params are Inf: "+str(inf_params))
+        if any([np.any(np.isnan(param.get_value())) for param in model.get_params()]):
+            nan_params = [ param for param in model.get_params() if np.any(np.isnan(param.get_value())) ]
+            raise ValueError("These params are NaN: "+str(nan_params))
+        self.model = model
+
+        batch_size = self.batch_size
+        if hasattr(model, "force_batch_size"):
+            if model.force_batch_size > 0:
+                if batch_size is not None:
+                    if batch_size != model.force_batch_size:
+                        if self.set_batch_size:
+                            model.set_batch_size(batch_size)
+                        else:
+                            raise ValueError("batch_size argument to SGD conflicts with model's force_batch_size attribute")
+                else:
+                    self.batch_size = model.force_batch_size
+        model._test_batch_size = self.batch_size
+        self.monitor = Monitor.get_monitor(model)
+        # TODO: come up with some standard scheme for associating training runs
+        # with monitors / pushing the monitor automatically, instead of just
+        # enforcing that people have called push_monitor
+        assert self.monitor.get_examples_seen() == 0
+        self.monitor._sanity_check()
+
+
+
+
+        X = model.get_input_space().make_theano_batch(name="%s[X]" % self.__class__.__name__)
+        self.topo = not X.ndim == 2
+
+        if config.compute_test_value == 'raise':
+            if self.topo:
+                X.tag.test_value = dataset.get_batch_topo(self.batch_size)
+            else:
+                X.tag.test_value = dataset.get_batch_design(self.batch_size)
+
+        Y = T.tensor3(name="%s[Y]" % self.__class__.__name__)
+
+
+        if self.cost.supervised:
+            if config.compute_test_value == 'raise':
+                _, Y.tag.test_value = dataset.get_batch_design(self.batch_size, True)
+
+            self.supervised = True
+            cost_value = self.cost(model, X, Y)
+
+        else:
+            self.supervised = False
+            cost_value = self.cost(model, X)
+        if cost_value is not None and cost_value.name is None:
+            if self.supervised:
+                cost_value.name = 'objective(' + X.name + ', ' + Y.name + ')'
+            else:
+                cost_value.name = 'objective(' + X.name + ')'
+
+        # Set up monitor to model the objective value, learning rate,
+        # momentum (if applicable), and extra channels defined by
+        # the cost
+        learning_rate = self.learning_rate
+        if self.monitoring_dataset is not None:
+            self.monitor.setup(dataset=self.monitoring_dataset,
+                    cost=self.cost, batch_size=self.batch_size, num_batches=self.monitoring_batches,
+                    extra_costs=self.monitoring_costs
+                    )
+            if self.supervised:
+                ipt = (X, Y)
+            else:
+                ipt = X
+            dataset_name = self.monitoring_dataset.keys()[0]
+            monitoring_dataset = self.monitoring_dataset[dataset_name]
+            #TODO: have Monitor support non-data-dependent channels
+            self.monitor.add_channel(name='learning_rate', ipt=ipt,
+                    val=learning_rate, dataset=monitoring_dataset)
+            if self.momentum:
+                self.monitor.add_channel(name='momentum', ipt=ipt,
+                        val=self.momentum, dataset=monitoring_dataset)
+
+        params = list(model.get_params())
+        assert len(params) > 0
+        for i, param in enumerate(params):
+            if param.name is None:
+                param.name = 'sgd_params[%d]' % i
+
+        if self.cost.supervised:
+            grads, updates = self.cost.get_gradients(model, X, Y)
+        else:
+            grads, updates = self.cost.get_gradients(model, X)
+
+        for param in grads:
+            assert param in params
+        for param in params:
+            assert param in grads
+
+        for param in grads:
+            if grads[param].name is None and cost_value is not None:
+                grads[param].name = ('grad(%(costname)s, %(paramname)s)' %
+                                     {'costname': cost_value.name,
+                                      'paramname': param.name})
+
+        lr_scalers = model.get_lr_scalers()
+
+        for key in lr_scalers:
+            if key not in params:
+                raise ValueError("Tried to scale the learning rate on " +\
+                        str(key)+" which is not an optimization parameter.")
+
+        log.info('Parameter and initial learning rate summary:')
+        for param in params:
+            param_name = param.name
+            if param_name is None:
+                param_name = 'anon_param'
+            lr = learning_rate.get_value() * lr_scalers.get(param,1.)
+            log.info('\t' + param_name + ': ' + str(lr))
+
+        if self.momentum is None:
+            updates.update( dict(safe_zip(params, [param - learning_rate * \
+                lr_scalers.get(param, 1.) * grads[param]
+                                    for param in params])))
+        else:
+            for param in params:
+                inc = sharedX(param.get_value() * 0.)
+                if param.name is not None:
+                    inc.name = 'inc_'+param.name
+                updated_inc = self.momentum * inc - learning_rate * lr_scalers.get(param, 1.) * grads[param]
+                updates[inc] = updated_inc
+                updates[param] = param + updated_inc
+
+
+        for param in params:
+            if updates[param].name is None:
+                updates[param].name = 'sgd_update(' + param.name + ')'
+        model.censor_updates(updates)
+        for param in params:
+            update = updates[param]
+            if update.name is None:
+                update.name = 'censor(sgd_update(' + param.name + '))'
+            for update_val in get_debug_values(update):
+                if np.any(np.isinf(update_val)):
+                    raise ValueError("debug value of %s contains infs" % update.name)
+                if np.any(np.isnan(update_val)):
+                    raise ValueError("debug value of %s contains nans" % update.name)
+
+
+        with log_timing(log, 'Compiling sgd_update'):
+            if self.supervised:
+                fn_inputs = [X, Y]
+            else:
+                fn_inputs = [X]
+            self.sgd_update = function(fn_inputs, updates=updates,
+                                       name='sgd_update',
+                                       on_unused_input='ignore',
+                                       mode=self.theano_function_mode)
+        self.params = params
---
+++ emotiw/abhi/kpd/Multisoftmax.py
@@ -0,0 +1,271 @@
+from pylearn2.space import VectorSpace, Conv2DSpace, CompositeSpace, Space
+from pylearn2.models.mlp import Layer
+from pylearn2.utils import py_integer_types, py_float_types
+from pylearn2.space import VectorSpace, Conv2DSpace, CompositeSpace, Space
+import functools
+from pylearn2.utils import sharedX
+import theano.tensor as T
+from theano import config
+import numpy as np
+from theano import function
+from theano.gof.op import get_debug_values
+from theano.printing import Print
+import theano
+
+from collections import OrderedDict
+
+
+def batched_softmax(x):
+    """
+    :param x: A Tensor3
+    This function computes the batched softmax
+    """
+                
+    result, updates = theano.scan(fn=lambda x_mat:
+            T.nnet.softmax(x_mat),
+            outputs_info=None,
+            sequences=[x],
+            non_sequences=None)
+    return result
+                
+class MatrixSpace(Space):
+    """A space whose points are defined as fixed-length vectors."""
+    def __init__(self, num_row, num_column):
+        """
+        Initialize a MatrixSpace.
+
+        Parameters
+        ----------
+        dim : int
+            Dimensionality of a vector in this space.
+        sparse: bool
+            Sparse vector or not
+        """
+        self.num_row = num_row
+        self.num_column = num_column
+
+    @functools.wraps(Space.get_origin)
+    def get_origin(self):
+        return np.zeros((self.num_row,self.num_column))
+
+    @functools.wraps(Space.get_origin_batch)
+    def get_origin_batch(self, n):
+        return np.zeros((n, self.num_row, self.num_column))
+
+    @functools.wraps(Space.make_theano_batch)
+    def make_theano_batch(self, name=None, dtype=None):
+        if dtype is None:
+            dtype = config.floatX
+
+        return T.tensor3(name=name, dtype=dtype)
+
+    @functools.wraps(Space.get_total_dimension)
+    def get_total_dimension(self):
+        return self.num_column * self.num_row
+
+    @functools.wraps(Space._format_as)
+    def _format_as(self, batch, space):
+        raise NotImplementedError()
+        if isinstance(space, CompositeSpace):
+            pos = 0
+            pieces = []
+            for component in space.components:
+                width = component.get_total_dimension()
+                subtensor = batch[:,pos:pos+width]
+                pos += width
+                formatted = VectorSpace(width).format_as(subtensor, component)
+                pieces.append(formatted)
+            return tuple(pieces)
+
+        if isinstance(space, Conv2DSpace):
+            if space.axes[0] != 'b':
+                raise NotImplementedError("Will need to reshape to ('b',*) then do a dimshuffle. Be sure to make this the inverse of space._format_as(x, self)")
+            dims = { 'b' : batch.shape[0], 'c' : space.num_channels, 0 : space.shape[0], 1 : space.shape[1] }
+
+            shape = tuple( [ dims[elem] for elem in space.axes ] )
+
+            rval = batch.reshape(shape)
+
+            return rval
+
+        raise NotImplementedError("VectorSpace doesn't know how to format as "+str(type(space)))
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.num_row == other.num_row and self.num_column == other.num_column
+
+    def validate(self, batch):
+        if not isinstance(batch, theano.gof.Variable):
+            raise TypeError("MatrixSpace batch should be a theano Variable, got "+str(type(batch)))
+        if batch.ndim != 3:
+            raise ValueError('MatrixSpace batches must be 2D, got %d dimensions' % batch.ndim)
+
+class MultiSoftmax(Layer):
+
+    def __init__(self, n_groups, n_classes, layer_name, irange = None,
+                 istdev = None, sparse_init = None, W_lr_scale = None,
+                 b_lr_scale = None, max_row_norm = None,
+                 no_affine = False, max_col_norm = None):
+        """
+        """
+
+        if isinstance(W_lr_scale, str):
+            W_lr_scale = float(W_lr_scale)
+
+        self.__dict__.update(locals())
+        del self.self
+
+        assert isinstance(n_classes, py_integer_types)
+
+        self.output_space = MatrixSpace(n_groups, n_classes)
+        self.b = sharedX( np.zeros((n_groups, n_classes,)), name = 'softmax_b')
+
+    def get_lr_scalers(self):
+
+        rval = OrderedDict()
+
+        if self.W_lr_scale is not None:
+            assert isinstance(self.W_lr_scale, float)
+            rval[self.W] = self.W_lr_scale
+
+        if not hasattr(self, 'b_lr_scale'):
+            self.b_lr_scale = None
+
+        if self.b_lr_scale is not None:
+            assert isinstance(self.b_lr_scale, float)
+            rval[self.b] = self.b_lr_scale
+
+        return rval
+
+    def get_monitoring_channels(self):
+        return OrderedDict()
+
+    def get_monitoring_channels_from_state(self, state, target=None):
+        return OrderedDict()
+        
+    def set_input_space(self, space):
+        self.input_space = space
+
+        if not isinstance(space, Space):
+            raise TypeError("Expected Space, got "+
+                    str(space)+" of type "+str(type(space)))
+
+        self.input_dim = space.get_total_dimension()
+        self.needs_reformat = not isinstance(space, VectorSpace)
+
+        if self.no_affine:
+            desired_dim = self.n_classes
+            assert self.input_dim == desired_dim
+        else:
+            desired_dim = self.input_dim
+        self.desired_space = VectorSpace(desired_dim)
+
+        if not self.needs_reformat:
+            assert self.desired_space == self.input_space
+
+        rng = self.mlp.rng
+
+        if self.irange is not None:
+            assert self.istdev is None
+            assert self.sparse_init is None
+            W = rng.uniform(-self.irange,self.irange, (self.input_dim,self.n_groups,self.n_classes))
+        elif self.istdev is not None:
+            assert self.sparse_init is None
+            W = rng.randn(self.input_dim,self.n_groups,self.n_classes) * self.istdev
+        else:
+            raise NotImplementedError()
+
+        self.W = sharedX(W,  'softmax_W' )
+
+        self._params = [ self.b, self.W ]
+
+    def get_weights_topo(self):
+        if not isinstance(self.input_space, Conv2DSpace):
+            raise NotImplementedError()
+        desired = self.W.get_value().T
+        ipt = self.desired_space.format_as(desired, self.input_space)
+        rval = Conv2DSpace.convert_numpy(ipt, self.input_space.axes, ('b', 0, 1, 'c'))
+        return rval
+
+    def get_weights(self):
+        if not isinstance(self.input_space, VectorSpace):
+            raise NotImplementedError()
+
+        return self.W.get_value()
+
+    def set_weights(self, weights):
+        self.W.set_value(weights)
+
+    def set_biases(self, biases):
+        self.b.set_value(biases)
+
+    def get_biases(self):
+        return self.b.get_value()
+
+    def get_weights_format(self):
+        return ('v', 'h')
+
+    def fprop(self, state_below):
+
+        self.input_space.validate(state_below)
+
+        if self.needs_reformat:
+            state_below = self.input_space.format_as(state_below, self.desired_space)
+
+        for value in get_debug_values(state_below):
+            if self.mlp.batch_size is not None and value.shape[0] != self.mlp.batch_size:
+                raise ValueError("state_below should have batch size "+str(self.dbm.batch_size)+" but has "+str(value.shape[0]))
+
+        self.desired_space.validate(state_below)
+        assert state_below.ndim == 2
+
+        assert self.W.ndim == 3
+
+        Z = T.tensordot(state_below, self.W, axes=[[1],[0]]) + self.b
+
+        rval = batched_softmax(Z)
+
+        for value in get_debug_values(rval):
+            if self.mlp.batch_size is not None:
+                assert value.shape[0] == self.mlp.batch_size
+
+        return rval
+
+    def cost(self, Y, Y_hat):
+        return self.cost_from_cost_matrix(self.cost_matrix(Y, Y_hat))
+
+    def cost_from_cost_matrix(self, cost_matrix):
+        return cost_matrix.sum(axis=2).mean()
+
+    def cost_matrix(self, Y, Y_hat):
+        return -Y * T.log(Y_hat+0.000001)
+
+    def get_weight_decay(self, coeff):
+        if isinstance(coeff, str):
+            coeff = float(coeff)
+        assert isinstance(coeff, float) or hasattr(coeff, 'dtype')
+        return coeff * T.sqr(self.W).sum()
+
+    def get_l1_weight_decay(self, coeff):
+        if isinstance(coeff, str):
+            coeff = float(coeff)
+        assert isinstance(coeff, float) or hasattr(coeff, 'dtype')
+        W = self.W
+        return coeff * abs(W).sum()
+
+    def censor_updates(self, updates):
+        return
+        if self.max_row_norm is not None:
+            W = self.W
+            if W in updates:
+                updated_W = updates[W]
+                row_norms = T.sqrt(T.sum(T.sqr(updated_W), axis=1))
+                desired_norms = T.clip(row_norms, 0, self.max_row_norm)
+                updates[W] = updated_W * (desired_norms / (1e-7 + row_norms)).dimshuffle(0, 'x')
+        if self.max_col_norm is not None:
+            assert self.max_row_norm is None
+            W = self.W
+            if W in updates:
+                updated_W = updates[W]
+                col_norms = T.sqrt(T.sum(T.sqr(updated_W), axis=0))
+                desired_norms = T.clip(col_norms, 0, self.max_col_norm)
+                updates[W] = updated_W * (desired_norms / (1e-7 + col_norms))
