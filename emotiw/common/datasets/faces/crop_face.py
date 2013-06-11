9e72e20e287243222e93ef90f276258ea2931acb
Jeremie Zumer <zumerjer@iro.umontreal.ca>
2013-06-11 11:42:01 -0400

    Better centering in crop_face. Added HDF5KeypointsWrapper: a pylearn2 wrapper to the datasets described in keypoints_desc.

--- emotiw/common/datasets/faces/crop_face.py
+++ emotiw/common/datasets/faces/crop_face.py
@@ -4,7 +4,7 @@ import numpy.random
 import math
 
 def crop_face(img, bbox, eyes, points={}):
-    img = img.convert("RGBX")
+    img = img.convert("RGB")
     bb = bbox
     x0, y0, x1, y1 = (None, None, None, None)
     to_remove = []
@@ -15,89 +15,54 @@ def crop_face(img, bbox, eyes, points={}):
 
     for tr in to_remove:
         del points[tr]
-    
-    if bb is None:
-        try:
-            x0, y0 = points['left_eyebrow_outer_end']
-            x1, _ = points['right_eyebrow_outer_end']
-        except KeyError:
-            try:
-                if x0 is None:
-                    x0, y0 = points['left_eyebrow_center']
-                x1, _ = points['right_eyebrow_center']
-            except KeyError:
-                try:
-                    if x0 is None:
-                        x0, y0 = points['left_eyebrow_inner_end']
-                    x1, _ = points['right_eyebrow_inner_end']
-                except KeyError:
-                    try:
-                        if y0 is None:
-                            _, y0 = points['left_eye_center']
-                        x1, _ = points['right_mouth_corner']
-                        
-                        if x0 is None:
-                            x0, _ = points['left_mouth_corner']
-                    except KeyError:
-                        if x0 is None:
-                            x0 = 0
-                        if y0 is None:
-                            y0 = 0                
-                        if x1 is None:
-                            x1 = img.size[0]-1
 
-        try:
-            _, y1 = points['chin_center']
-        except KeyError:
-            try:
-                _, y1 = points['mouth_center']
-            except KeyError:
-                y1 = img.size[1]-1
+    if bb is None:
+        right = -1
+        left = img.size[0]+1
+        top = -1
+        bottom = img.size[1]+1
+        
+        for pt in points:
+            right = max(right, points[pt][0])
+            left = min(left, points[pt][0])
+            bottom = min(bottom, points[pt][1])
+            top = max(top, points[pt][1])
+
+        x0 = left
+        x1 = right
+        y0 = top
+        y1 = bottom
 
     else:
         x0, y0, x1, y1 = tuple(bb)
-
-    x_delta = None #look to the sides more than the keypoints would suggest
-    y_delta = None    
 
     x0 = float(x0)
     x1 = float(x1)
     y0 = float(y0)
     y1 = float(y1)
 
-    try:
-        x_delta = eyes[2] - eyes[0]
-        y_delta = eyes[3] - eyes[1]
-
-    except TypeError:
-        try:
-            y_delta = eyes[3] - eyes[1]
-        except TypeError:
-            if x_delta is None:
-                x_delta = img.size[0]/5.0
-            if y_delta is None:
-                y_delta = img.size[1]/5.0
-        if x_delta is None:
-            x_delta = img.size[0]/5.0
-
-    x_diff = abs(x1 - x0 + x_delta)
-    y_diff = abs(y1 - y0 + y_delta)
-    side = min(x_diff + x_delta, y_diff + y_delta)
+    side_x = abs(x1 - x0)
+    side_y = abs(y1 - y0)
+    the_side = max(1.3*side_x, 1.3*side_y)
     
-    x0 = max(min(x0, x1) - side/2.0, 0.0)
-    x1 = min(x0 + 2.0*side, img.size[0])
-    y0 = max(min(y0, y1) - side/2.0, 0.0)
-    y1 = min(y0 +  2.0*side, img.size[1])
+    side_x = min(img.size[0], the_side)
+    side_y = min(img.size[1], the_side)
 
-    side_0 = x1 - x0
-    side_1 = y1 - y0
+    x0 = max(min(x0, x1) - side_x/2.0, 0)
+    x1 = min(x0 + 1.5*side_x, img.size[0])
+    y0 = max(min(y0, y1) - side_y/2.0, 0)
+    y1 = min(y0 + 1.5*side_y, img.size[1])
 
-    clamped = [(p, ((96.0*(points[p][0] - x0))/(side_0), (96.0*(points[p][1] - y0))/(side_1))) for p in points if x1 > points[p][0] > x0 and y1 > points[p][1] > y0]
+    side_x = x1 - x0
+    side_y = y1 - y0
+    the_side = min(side_x, side_y)
+
+    clamped = [(p, ((96.0*(points[p][0] - x0))/(the_side), (96.0*(points[p][1] - y0))/(the_side))) for p in points if x1 >= points[p][0] >= x0 and y1 >= points[p][1] >= y0]
 
     new_points = {}
     for c in clamped:
         new_points[c[0]] = c[1]
-    
+
     method = None
     if img.size[0] < 96 or img.size[1] < 96:
         method = PIL.Image.ANTIALIAS
@@ -113,20 +78,26 @@ def crop_face(img, bbox, eyes, points={}):
 def display_1(ds, idx):
     img, pts = crop_face(PIL.Image.open(ds.get_original_image_path(idx)), ds.get_bbox(idx), ds.get_eyes_location(idx), ds.get_keypoints_location(idx))
     img = img.convert("RGBA")
+    img0 = PIL.Image.open(ds.get_original_image_path(idx))
 
     data_str = ""
         
     for y in xrange(96):
         for x in xrange(96):
-            if (float(x), float(y)) in pts:
-                data_str += '\x00\x00\x00\x00'
-            else:
+            found = False
+            for p in pts:
+                if int(pts[p][0]) == x and int(pts[p][1]) == y:
+                    data_str += '\x00\x00\x00\x00'
+                    found = True
+                    break
+            if not found:
                 data_str += '\x00\x00\x00\xff'
 
     pt_img = PIL.Image.fromstring(mode="RGBA", size=(96, 96), data=data_str)
     img = PIL.Image.composite(img, pt_img, pt_img)
+    img0.paste(img, (img0.size[0]-96, img0.size[1]-96, img0.size[0], img0.size[1]))
     
-    img.show()
+    img0.show()
 
 def display_n(ds, n):
     for i in xrange(min(n, len(ds))):
