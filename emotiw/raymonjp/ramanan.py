658cbbd21880693f884ede584cf5af78f73ea371
jpraymond <raymonjp@iro.umontreal.ca>
2013-07-09 17:23:21 -0400

    Added Ramanan's MATLAB (and c++) code

--- emotiw/raymonjp/ramanan.py
+++ emotiw/raymonjp/ramanan.py
@@ -2,4 +2,13 @@
 Created on 2013-07-09
 
 @author: "JP Raymond(raymonjp@iro)
+
+Dependencies ...
 '''
+
+from mlabwrap import mlab
+mlab.demoneimagewhole('/data/lisa/exp/raymonjp/Ramanan/ToDo/Angry/',
+                      '/data/lisa/exp/raymonjp/Ramanan/Results/',
+                      '/data/lisa/exp/raymonjp/Ramanan/Done/')
+
+# def ramanan():
