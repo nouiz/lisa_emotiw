99eacab464170a2ec553dcfc3aed33b5a7b40f73
Abhishek Aggarwal <abhiggarwal@gmail.com>
2013-07-17 11:07:37 -0400

    single frame classifier working

--- emotiw/abhi/classifier/singleFrame_mlp.py
+++ emotiw/abhi/classifier/singleFrame_mlp.py
@@ -1,40 +1,44 @@
 from pylearn2.train import Train
 from emotiw.common.datasets.faces.afew2_facetubes import AFEW2FaceTubes
-from pylearn2.model.mlp import MLP, Softmax
+from pylearn2.models.mlp import MLP, Softmax
 from pylearn2.models.maxout import MaxoutConvC01B, Maxout
 from pylearn2.space import Conv2DSpace
+#from emotiw.common.datasets.faces.facetubes import FaceTubeSpace
 from pylearn2.training_algorithms.sgd import SGD, ExponentialDecay, MomentumAdjustor
 from pylearn2.costs.mlp.dropout import Dropout
 from pylearn2.termination_criteria import MonitorBased
 from pylearn2.train_extensions.best_params import MonitorBasedSaveBest
+from emotiwFrameDataset import EmotiwFrameDataset
 
 save_path= "last.pkl"
 save_best_path = 'best.pkl'
 save_freq= 10
+num_chan = 1
+splitRatio = 0.7
+batch_size = 1000
 
-
-
-train_ds = AFEW2FaceTubes(
-        which_set= 'train',
-        one_hot= 1,
-        preproc= ['smooth'],
-        size= [48, 48])
+train_ds = EmotiwFrameDataset(which_set= 'train',
+                         one_hot= 1,
+                         splitRatio = splitRatio,
+                         size= [48, 48],
+                         num_channels = num_chan)
 
 print 'Train Dataset Loaded'
 
-val_ds = AFEW2FaceTubes(
-         which_set= 'val',
-         one_hot= 1,
-         preproc= ['smooth'],
-         size= [48, 48])
+val_ds = EmotiwFrameDataset(which_set= 'val',
+                         one_hot= 1,
+                         splitRatio = splitRatio,
+                         size= [48, 48],
+                         num_channels = num_chan)
 
 print 'Val Dataset Loaded'
 last_ndim = 240
 n_classes = 7
 
 
+
 algorithm= SGD(
-        batch_size= 1,
+        batch_size= batch_size,
         learning_rate= 0.000100,
         init_momentum= .5,
         monitoring_dataset= {'valid' : val_ds},
@@ -58,10 +62,11 @@ extensions = [MonitorBasedSaveBest(
             saturate= 250,
             final_momentum= .7)]
     
-model = MLP(input_space = Conv2DSpace (
+model = MLP(batch_size = batch_size,
+            input_space = Conv2DSpace(
             shape= [48, 48],
-            num_channels= 3,
-            axes= ['b', 't', 0, 1, 'c']
+            num_channels= num_chan,
+            axes= ['c', 0, 1, 'b']
             ),
              layers= [MaxoutConvC01B(
                      layer_name= 'h0',
@@ -107,3 +112,5 @@ train = Train(
     save_path= save_path,
     save_freq=save_freq
 )
+
+train.main_loop()
