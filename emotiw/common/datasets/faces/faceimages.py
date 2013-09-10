0df0be2d84a87ad39618b1978bf5fdb470955704
Pascal Vincent <vincentp@iro.umontreal.ca>
2013-09-10 15:07:11 -0400

    Put back the loading of images

--- emotiw/common/datasets/faces/faceimages.py
+++ emotiw/common/datasets/faces/faceimages.py
@@ -32,7 +32,7 @@ import os
 import os.path
 import glob
 import sys
-#import cv
+import cv
 import Image
 import ImageDraw
 import ImageFont
@@ -105,10 +105,10 @@ class FaceImagesDataset(object):
 
     def get_original_image(self, i):
         filepath = self.get_original_image_path(i)
-        print 'check the code, its been compromised'
-        #img = cv.LoadImage(filepath)
-        #return img
-        return None
+        # print 'check the code, its been compromised'
+        # return None
+        img = cv.LoadImage(filepath)
+        return img
 
     def get_original_image_path(self,i):
         """
