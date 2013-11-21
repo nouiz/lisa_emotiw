b175ed22c12916bf73c2dee86177e246e9701dfd
Raul Chandias Ferrari <chandiar@iro.umontreal.ca>
2013-11-21 18:15:30 -0500

    Command line for scripts

--- scripts/chandiar/smoothed_face_tube.py
+++ scripts/chandiar/smoothed_face_tube.py
@@ -7,18 +7,21 @@ from conf import config
 
 
 def main():
-    import pdb; pdb.set_trace()
+    extracted_frames_path = sys.argv[1]
+    picasa_bbox_path = sys.argv[2]
+    save_path = sys.argv[3]
+
     to_be_saved = []
     missing_bbx = []
-    clip_id = config['extracted_frames_path'].split('/')[-1]
+    clip_id = extracted_frames_path.split('/')[-1]
     if True:
         print 'clip id: ', clip_id
-        frames = get_frames("{}/".format(config['extracted_frames_path']), [clip_id])
-        bounding_boxes = get_bounding_boxes("{}/".format(config['picasa_bbox_path']), [clip_id])
+        frames = get_frames("{}/".format(extracted_frames_path), [clip_id])
+        bounding_boxes = get_bounding_boxes("{}/".format(picasa_bbox_path), [clip_id])
         failed = []
         face_tubes = get_face_tubes(frames, bounding_boxes, failed, 
-                                    "{}/".format(config['extracted_frames_path']), 
-                                    "{}/".format(config['save_path']), 
+                                    "{}/".format(extracted_frames_path), 
+                                    "{}/".format(save_path), 
                                     config['distance_thr'], 
                                     config['size_thre'], 
                                     config['overlap_thr'], 
@@ -26,8 +29,8 @@ def main():
 
         if config['what_to_save'] in ['img', 'mat']:
             save_tube(face_tubes, 
-                      "{}/".format(config['extracted_frames_path']), 
-                      "{}/".format(config['save_path']), 
+                      "{}/".format(extracted_frames_path), 
+                      "{}/".format(save_path), 
                       config['size'],
                       config['what_to_save'])
         else:
