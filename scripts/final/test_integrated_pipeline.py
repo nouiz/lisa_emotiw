3e03f5de59953aaaa95612cca597066523900a5f
David Krueger <davidscottkrueger@gmail.com>
2013-11-25 15:56:45 -0500

    dk commits

--- scripts/final/test_integrated_pipeline.py
+++ scripts/final/test_integrated_pipeline.py
@@ -76,11 +76,14 @@ frame_dir = os.path.join(DATA_ROOT_DIR, 'extracted_frames')
 # Use Ramanan keypoints algorithm... finds and saves detections. 
 # Needs to check it PICASSA failed!
 # How to deal with multiple detections? (currently only saves the best)
+print "Phase 2.1b", '\n', "script_dir = ", script_dir
+
 backup_faces_dir = os.path.join(DATA_ROOT_DIR, 'ramanan_keypoints_picassa_backup')
 if not os.path.exists(backup_faces_dir):
     os.mkdir(backup_faces_dir)
 
-count = 0
+script_dir = os.path.join(SCRIPTS_PATH, 'samira/RamananCodes')
+
 
 for clip_id in CLIP_IDS:
     this_clip_frame_dir = os.path.join(frame_dir, clip_id)
@@ -88,21 +91,75 @@ for clip_id in CLIP_IDS:
     clips = []
     for i,j,c in os.walk(this_clip_frame_dir):
         clips = c
-    for clip in clips:
-        if count == 0:
-            print '\n', clip, this_clip_frame_dir, this_clip_backup_faces_dir, '\n'
-            dest_dir = this_clip_backup_faces_dir + '/' + clip[:-4] + '__ramanan.mat'
-            clip_dir = this_clip_frame_dir + '/' + clip
-            print clip_dir, dest_dir, '\n'
+        #print '\n', clips, this_clip_frame_dir, this_clip_backup_faces_dir, '\n'
+        if not os.path.exists(this_clip_frame_dir):
+            os.mkdir(this_clip_frame_dir)
+        if not os.path.exists(this_clip_backup_faces_dir):
+            os.mkdir(this_clip_backup_faces_dir)
+        print "demoneim filepaths = ", this_clip_frame_dir, this_clip_backup_faces_dir, '\n'
+        current_dir = os.getcwd()
+        samira_model_dir = '/data/lisa/exp/faces/emotiw_final/Samira'
+        cmd_line_template = 'bash %(script)s %(frame_dir)s %(backup_faces_dir)s %(scriptdir)s %(currentdir)s'
+        print "script_dir = ", script_dir
+        for clip_id in CLIP_IDS:
+            cmd_line = cmd_line_template % dict(
+                script=os.path.join(SCRIPTS_PATH, 'samira/RamananCodes', 'demoneimagewhole_alt.bash'),
+                frame_dir=this_clip_frame_dir,
+                backup_faces_dir=this_clip_backup_faces_dir,
+                scriptdir = script_dir,
+                currentdir = current_dir)
+        print '\n', 'executing cmd:'
+        print cmd_line, '\n', '\n', '\n', '\n'
+        subprocess.check_call(cmd_line, shell=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#backup_faces_dir = os.path.join(DATA_ROOT_DIR, 'ramanan_keypoints_picassa_backup')
+#if not os.path.exists(backup_faces_dir):
+#    os.mkdir(backup_faces_dir)
+
+#count = 0
+
+#for clip_id in CLIP_IDS:
+ #   this_clip_frame_dir = os.path.join(frame_dir, clip_id)
+ #   this_clip_backup_faces_dir = os.path.join(backup_faces_dir, clip_id)
+  #  clips = []
+   ## for i,j,c in os.walk(this_clip_frame_dir):
+    #    clips = c
+     #   #print '\n', clips, this_clip_frame_dir, this_clip_backup_faces_dir, '\n'
+      ##  if not os.path.exists(this_clip_frame_dir):
+      #      os.mkdir(this_clip_frame_dir)
+      #  if not os.path.exists(this_clip_backup_faces_dir):
+      #      os.mkdir(this_clip_backup_faces_dir)
+      ##  print "demoneim filepaths = ", this_clip_frame_dir, this_clip_backup_faces_dir, '\n'
+       # mlab.demoneimagewhole_scaled(this_clip_frame_dir, this_clip_backup_faces_dir)
+    #
+   # for clip in clips:
+   #     if count != 0:
+   #         dest_dir = this_clip_backup_faces_dir + '/' + clip[:-4] + '__ramanan.mat'
+   ##         clip_dir = this_clip_frame_dir + '/' + clip
+    #        print clip_dir, dest_dir, '\n'
             #model can be 0 (used for challenge), 1, or 2.  
             #Lower numbered models are better and slower.
-            mlab.ramanan1(clip_dir, dest_dir, 0)
+    #        mlab.ramanan1(clip_dir, dest_dir, 0)
         #count += 1
 
 
 
 ## Phase 2.2b
-print "phase 2.2b"
+print '\n', '\n', "phase 2.2b",  '\n', '\n',
 #script_name = os.path.join(SCRIPTS_PATH, 'chandiar/missing_clips', 'get_bbox.py')
 #print script_name, type(script_name), str(script_name)
 
@@ -114,8 +171,10 @@ for clip_id in CLIP_IDS:
     this_clip_frame_dir = os.path.join(frame_dir, clip_id)
 #    this_clip_backup_faces_dir = os.path.join(backup_faces_dir, clip_id)
     this_clip_backup_faces_dir = backup_faces_dir
+    if not os.path.exists(this_clip_backup_faces_dir):
+        os.mkdir(this_clip_backup_faces_dir)
     print 'getbbox inputs = ', this_clip_frame_dir, this_clip_backup_faces_dir, backup_bboxes_dir
-    get_bbox(this_clip_frame_dir, this_clip_backup_faces_dir, backup_bboxes_dir)
+    get_bbox(this_clip_frame_dir, this_clip_backup_faces_dir, backup_bboxes_dir, backup_bboxes_dir)
 
 
 
---
+++ scripts/samira/RamananCodes/demoneimagewhole_alt.m
@@ -0,0 +1,78 @@
+function demoneimagewhole_alt(path_in,path_out)
+%path_in='C:\challenges\FaceTubes\missingtestvideos\DoneMissingtestvideos\';
+%path_out='C:\challenges\FaceTubes\missingtestvideos\test\';
+'scaled'
+
+imagedir=dir(fullfile(path_in, '*.png'));
+%path_=['images/' imagedir(kk).name];
+%path_res=['Result/' imagedir(kk).name];
+%path_done=['Done/' imagedir(kk).name];
+
+%compile;
+% load and visualize model
+% Pre-trained model with 146 parts. Works best for faces larger than 80*80
+% load face_p146_small.mat
+
+% % Pre-trained model with 99 parts. Works best for faces larger than 150*150
+% load face_p99.mat
+
+% % Pre-trained model with 1050 parts. Give best performance on localization, but very slow
+load multipie_independent.mat
+
+disp('Model visualization');
+%visualizemodel(model,1:13);
+
+% 5 levels for each octave
+model.interval = 5;
+% set up the threshold
+model.thresh = min(-0.8, model.thresh);
+% define the mapping from view-specific mixture id to viewpoint
+if length(model.components)==13
+    posemap = 90:-15:-90;
+elseif length(model.components)==18
+    posemap = [90:-15:15 0 0 0 0 0 0 -15:-15:-90];
+else
+    error('Can not recognize this model');
+end
+
+for i=1:length(imagedir)
+    k=imagedir(i).name;
+    img=imread([path_in '/' k]);
+    
+    if size(img,3)==1
+        im = repmat(img,[1 1 3]);
+    else
+        im = img;
+    end
+    
+    imsize = size(im);
+    scl =  250/min(imsize(1),imsize(2))
+    %scl = 2;
+    im = imresize(im,scl);
+    size(im)
+    
+    tic;
+    bs = detect(im, model, model.thresh);
+    bs = clipboxes(im, bs);
+    bs = nms_face(bs,0.3);
+    dettime = toc;
+    
+    if size(bs,1)>0
+        % show highest scoring one
+        [xs,ys]=showboxes(im, bs,posemap);title('Highest scoring detection');
+        xs = xs/scl; ys = ys/scl;
+        path_res=[path_out '/' k];
+        save(strcat(path_res(1:end-4),'.mat'),'xs','ys','bs','dettime','posemap');
+        for kk=1:size(xs,2)
+            img(int16(ys(kk)),int16(xs(kk)),:)=255;
+        end
+        imwrite(img,[path_out '/' k]);
+        % show all
+        %figure,showboxes(im, bs,posemap);title('All detections above the threshold');
+        fprintf('Detection took %.1f seconds\n',dettime);
+        close all;
+    end
+    %movefile(path_,path_done);
+end
+disp('done!');
+end
